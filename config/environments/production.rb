aad4de588f8d1a680a049f5eee60bb275134b669
Sarah Hanson <sarahh.hanson@gmail.com>
2018-12-20 16:15:30 -0600

    added default_url_options for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "my-app_#{Rails.env}"
+  config.action_mailer.default_url_options = { host: 'meet-planner.herokuapp.com' }
 
   config.action_mailer.perform_caching = false
 
