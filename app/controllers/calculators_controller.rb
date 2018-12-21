b6b1bc6979fc2b3b8ec3241b4eb94c8de8fb4a50
Sarah Hanson <sarahh.hanson@gmail.com>
2018-12-21 17:49:44 -0600

    added masters sinclair calc function

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,2 +1,3 @@
 class CalculatorsController < ApplicationController
+  skip_before_action :authenticate_user!
 end
--- app/javascript/components/calculator/App.js
+++ app/javascript/components/calculator/App.js
@@ -13,6 +13,26 @@ const coefficients = {
   }
 }
 
+const pad = new Array(30).fill(null);
+const mastersCoefficients = [...pad,
+  1.000, 1.016, 1.031, 1.046,
+  1.059, 1.072, 1.083, 1.096,
+  1.109, 1.122, 1.135, 1.149,
+  1.162, 1.176, 1.189, 1.203,
+  1.218, 1.233, 1.248, 1.263,
+  1.279, 1.297, 1.316, 1.338,
+  1.361, 1.385, 1.411, 1.437,
+  1.462, 1.488, 1.514, 1.541,
+  1.568, 1.598, 1.629, 1.663,
+  1.699, 1.738, 1.779, 1.823,
+  1.867, 1.910, 1.953, 2.004,
+  2.060, 2.117, 2.181, 2.255,
+  2.336, 2.419, 2.504, 2.597,
+  2.702, 2.831, 2.981, 3.153,
+  3.352, 3.580, 3.843, 4.145,
+  4.493
+];
+
 class App extends React.Component {
   constructor() {
     super()
@@ -73,7 +93,16 @@ class App extends React.Component {
   }
 
   mastersTotal(){
-    return parseFloat(this.state.weight) * parseFloat(this.state.total) * parseFloat(this.state.age)
+    const coefficient = coefficients[this.state.gender];
+    let mastersTotal = this.state.total;
+
+    if (this.state.weight <= coefficient.b) {
+      const X = Math.log10(this.state.weight / coefficient.b);
+      const SC = Math.pow(10, coefficient.A * Math.pow(X, 2));
+      mastersTotal = this.state.total * SC * mastersCoefficients[this.state.age];
+    }
+
+     return mastersTotal.toFixed(2);
   }
 
   handleInputChange(event) {
