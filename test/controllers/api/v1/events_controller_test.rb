827af57b579287b832001130aa6364d427b36b10
Gabriel Odess Gillett <gabeodess@gmail.com>
2019-01-28 14:17:22 -0600

    tests passing

--- test/controllers/api/v1/events_controller_test.rb
+++ test/controllers/api/v1/events_controller_test.rb
@@ -1,13 +1,31 @@
 require 'test_helper'
 
 class Api::V1::EventsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get create' do
-    get api_v1_events_create_url
-    assert_response :success
+  test 'should create' do
+    @user = FactoryBot.create(:user)
+    sign_in(@user)
+    assert_difference '@user.events.count' do
+      post api_v1_events_url, params: { event: {
+        title: 'foobar',
+        fee: 0,
+        start_on: Date.today,
+        sanction_id: :foobar,
+        organizer: :foobar,
+        description: :foobar,
+        street: :foobar,
+        city: :foobar,
+        state: :foobar,
+        zipcode: '01060',
+        phone: '413-323-2343',
+        email: 'foo@bar.com'
+      } }
+      assert_response :success
+    end
   end
 
   test 'should get index' do
-    get api_v1_events_index_url
+    sign_in(FactoryBot.create(:user))
+    get api_v1_events_url
     assert_response :success
   end
 end
